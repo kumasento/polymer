f4d3c8e55ad7aca56a0e5faf64a0239a38d673bf
Ruizhe Zhao <vincentzhaorz@gmail.com>
2021-04-11 11:33:43 +0000

    Updated 2mm code

--- example/polybench/split-and-merge/2mm/2mm.split2.pluto.c
+++ example/polybench/split-and-merge/2mm/2mm.split2.pluto.c
@@ -4,15 +4,6 @@
 #define max(x,y)    ((x) > (y)? (x) : (y))
 #define min(x,y)    ((x) < (y)? (x) : (y))
 
-// TODO: mlir-clang %s %stdinclude | FileCheck %s
-// RUN: clang %s -O3 %stdinclude %polyverify -o %s.exec1 && %s.exec1 &> %s.out1
-// RUN: mlir-clang %s %polyverify %stdinclude -emit-llvm | clang -x ir - -O3 -o
-// %s.execm && %s.execm &> %s.out2 RUN: rm -f %s.exec1 %s.execm RUN: diff
-// %s.out1 %s.out2 RUN: rm -f %s.out1 %s.out2 RUN: mlir-clang %s %polyexec
-// %stdinclude -emit-llvm | clang -x ir - -O3 -o %s.execm && %s.execm >
-// %s.mlir.time; cat %s.mlir.time | FileCheck %s --check-prefix EXEC RUN: clang
-// %s -O3 %polyexec %stdinclude -o %s.exec2 && %s.exec2 > %s.clang.time; cat
-// %s.clang.time | FileCheck %s --check-prefix EXEC RUN: rm -f %s.exec2 %s.execm
 /**
  * This version is stamped on May 10, 2016
  *
@@ -24,10 +15,10 @@
  */
 /* 2mm.c: this file is part of PolyBench/C */
 
-#include <math.h>
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <math.h>
 
 /* Include polybench common header. */
 #include <polybench.h>
@@ -35,59 +26,69 @@
 /* Include benchmark-specific header. */
 #include "2mm.h"
 
+
 /* Array initialization. */
-static void init_array(int ni, int nj, int nk, int nl, DATA_TYPE *alpha,
-                       DATA_TYPE *beta,
-                       DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
-                       DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
-                       DATA_TYPE POLYBENCH_2D(C, NJ, NL, nj, nl),
-                       DATA_TYPE POLYBENCH_2D(D, NI, NL, ni, nl)) {
+static
+void init_array(int ni, int nj, int nk, int nl,
+		DATA_TYPE *alpha,
+		DATA_TYPE *beta,
+		DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj),
+		DATA_TYPE POLYBENCH_2D(C,NJ,NL,nj,nl),
+		DATA_TYPE POLYBENCH_2D(D,NI,NL,ni,nl))
+{
   int i, j;
 
   *alpha = 1.5;
   *beta = 1.2;
   for (i = 0; i < ni; i++)
     for (j = 0; j < nk; j++)
-      A[i][j] = (DATA_TYPE)((i * j + 1) % ni) / ni;
+      A[i][j] = (DATA_TYPE) ((i*j+1) % ni) / ni;
   for (i = 0; i < nk; i++)
     for (j = 0; j < nj; j++)
-      B[i][j] = (DATA_TYPE)(i * (j + 1) % nj) / nj;
+      B[i][j] = (DATA_TYPE) (i*(j+1) % nj) / nj;
   for (i = 0; i < nj; i++)
     for (j = 0; j < nl; j++)
-      C[i][j] = (DATA_TYPE)((i * (j + 3) + 1) % nl) / nl;
+      C[i][j] = (DATA_TYPE) ((i*(j+3)+1) % nl) / nl;
   for (i = 0; i < ni; i++)
     for (j = 0; j < nl; j++)
-      D[i][j] = (DATA_TYPE)(i * (j + 2) % nk) / nk;
+      D[i][j] = (DATA_TYPE) (i*(j+2) % nk) / nk;
 }
+
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static void print_array(int ni, int nl,
-                        DATA_TYPE POLYBENCH_2D(D, NI, NL, ni, nl)) {
+static
+void print_array(int ni, int nl,
+		 DATA_TYPE POLYBENCH_2D(D,NI,NL,ni,nl))
+{
   int i, j;
 
   POLYBENCH_DUMP_START;
   POLYBENCH_DUMP_BEGIN("D");
   for (i = 0; i < ni; i++)
     for (j = 0; j < nl; j++) {
-      if ((i * ni + j) % 20 == 0)
-        fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, D[i][j]);
+	if ((i * ni + j) % 20 == 0) fprintf (POLYBENCH_DUMP_TARGET, "\n");
+	fprintf (POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, D[i][j]);
     }
   POLYBENCH_DUMP_END("D");
   POLYBENCH_DUMP_FINISH;
 }
 
+
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
-static void kernel_2mm(unsigned int ni, unsigned int nj, unsigned int nk,
-                       unsigned int nl, DATA_TYPE alpha, DATA_TYPE beta,
-                       DATA_TYPE POLYBENCH_2D(tmp, NI, NJ, ni, nj),
-                       DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
-                       DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
-                       DATA_TYPE POLYBENCH_2D(C, NJ, NL, nj, nl),
-                       DATA_TYPE POLYBENCH_2D(D, NI, NL, ni, nl),
-                       DATA_TYPE POLYBENCH_1D(S, NJ, nj)) {
+static
+void kernel_2mm(int ni, int nj, int nk, int nl,
+		DATA_TYPE alpha,
+		DATA_TYPE beta,
+		DATA_TYPE POLYBENCH_2D(tmp,NI,NJ,ni,nj),
+		DATA_TYPE POLYBENCH_2D(A,NI,NK,ni,nk),
+		DATA_TYPE POLYBENCH_2D(B,NK,NJ,nk,nj),
+		DATA_TYPE POLYBENCH_2D(C,NJ,NL,nj,nl),
+		DATA_TYPE POLYBENCH_2D(D,NI,NL,ni,nl),
+    DATA_TYPE POLYBENCH_1D(S,NJ,nj))
+{
   int i, j, k;
 
   int t1, t2, t3, t4, t5, t6, t7, t8;
@@ -214,9 +215,12 @@ if (_PB_NI >= 1) {
     }
   }
 }
+
 }
 
-int main(int argc, char **argv) {
+
+int main(int argc, char** argv)
+{
   /* Retrieve problem size. */
   int ni = NI;
   int nj = NJ;
@@ -226,24 +230,32 @@ int main(int argc, char **argv) {
   /* Variable declaration/allocation. */
   DATA_TYPE alpha;
   DATA_TYPE beta;
-  POLYBENCH_2D_ARRAY_DECL(tmp, DATA_TYPE, NI, NJ, ni, nj);
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NI, NK, ni, nk);
-  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, NK, NJ, nk, nj);
-  POLYBENCH_2D_ARRAY_DECL(C, DATA_TYPE, NJ, NL, nj, nl);
-  POLYBENCH_2D_ARRAY_DECL(D, DATA_TYPE, NI, NL, ni, nl);
-  POLYBENCH_1D_ARRAY_DECL(S, DATA_TYPE, NJ, nj);
+  POLYBENCH_2D_ARRAY_DECL(tmp,DATA_TYPE,NI,NJ,ni,nj);
+  POLYBENCH_2D_ARRAY_DECL(A,DATA_TYPE,NI,NK,ni,nk);
+  POLYBENCH_2D_ARRAY_DECL(B,DATA_TYPE,NK,NJ,nk,nj);
+  POLYBENCH_2D_ARRAY_DECL(C,DATA_TYPE,NJ,NL,nj,nl);
+  POLYBENCH_2D_ARRAY_DECL(D,DATA_TYPE,NI,NL,ni,nl);
+  POLYBENCH_1D_ARRAY_DECL(S,DATA_TYPE,NJ,nj);
 
   /* Initialize array(s). */
-  init_array(ni, nj, nk, nl, &alpha, &beta, POLYBENCH_ARRAY(A),
-             POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C), POLYBENCH_ARRAY(D));
+  init_array (ni, nj, nk, nl, &alpha, &beta,
+	      POLYBENCH_ARRAY(A),
+	      POLYBENCH_ARRAY(B),
+	      POLYBENCH_ARRAY(C),
+	      POLYBENCH_ARRAY(D));
 
   /* Start timer. */
   polybench_start_instruments;
 
   /* Run kernel. */
-  kernel_2mm(ni, nj, nk, nl, alpha, beta, POLYBENCH_ARRAY(tmp),
-             POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B), POLYBENCH_ARRAY(C),
-             POLYBENCH_ARRAY(D), POLYBENCH_ARRAY(S));
+  kernel_2mm (ni, nj, nk, nl,
+	      alpha, beta,
+	      POLYBENCH_ARRAY(tmp),
+	      POLYBENCH_ARRAY(A),
+	      POLYBENCH_ARRAY(B),
+	      POLYBENCH_ARRAY(C),
+	      POLYBENCH_ARRAY(D),
+        POLYBENCH_ARRAY(S));
 
   /* Stop and print timer. */
   polybench_stop_instruments;
@@ -251,7 +263,7 @@ int main(int argc, char **argv) {
 
   /* Prevent dead-code elimination. All live-out data must be printed
      by the function call in argument. */
-  polybench_prevent_dce(print_array(ni, nl, POLYBENCH_ARRAY(D)));
+  polybench_prevent_dce(print_array(ni, nl,  POLYBENCH_ARRAY(D)));
 
   /* Be clean. */
   POLYBENCH_FREE_ARRAY(tmp);
